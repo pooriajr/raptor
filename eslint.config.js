0d155a5a25bf5017c5b934b126d2e068c5ec21e3
Pooria <pjrashidi@gmail.com>
2025-12-08 12:15:18 -0700

    Allow underscore-prefixed unused vars in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,12 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      // Allow unused vars/params prefixed with underscore (common convention for intentionally unused)
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
   },
 ])
